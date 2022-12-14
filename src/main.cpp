20a64522e1a41b8a25a19535d3d63d3b03356f46
St@rLeG <oleg.starynski@gmail.com>
2022-12-14 06:28:20 +0300

    update

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,21 @@
 int main()
 {
 	std::ifstream file("film.json");
- 	if(!file.is_open())
+	if (!file.is_open())
 	{
-		 std::cerr << "Error open file!!!" << std::endl;
+		std::cerr << "Error open file!!!" << std::endl;
 		return 1;
 	}
 
 	nlohmann::json j;
 
-	 file >> j;
-	 std::cout << j;
+	file >> j;
 
+	std::cout << j["Terminator 2"] << std::endl;
+	std::cout << j["Avatar"] << std::endl;
+	std::cout << j["Ghostbusters"] << std::endl;
+	std::cout << j["Dune"] << std::endl;
+	std::cout << j["Aliens"] << std::endl;
 
 	file.close();
 	return 0;
